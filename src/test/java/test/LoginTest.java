40ecb26dc71e3108a183d7242ace35d59c6b5699
TQTien <tien.tq200540@sis.hust.edu.vn>
2022-07-23 22:05:40 +0700

    Fix LoginTest

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -23,7 +23,7 @@ public class LoginTest extends BaseClassTest {
 		
 		Gson g = new Gson();
         LoginResponse rp = g.fromJson(res.asString(), LoginResponse.class);
-        rp.data.user.setPassword(password);
+        if (rp.data != null) rp.data.user.setPassword(password);
 		return rp;
 	}
 
